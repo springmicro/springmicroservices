ecc8f1c977469fb024c1d75cf3ddc30edf363ead
springmicro <springmicroservice@gmail.com>
2017-09-28 09:15:31 -0400

    latest

--- itemsharing.itemservice/src/main/java/com/itemsharing/itemservice/ItemserviceApplication.java
+++ itemsharing.itemservice/src/main/java/com/itemsharing/itemservice/ItemserviceApplication.java
@@ -1,12 +1,52 @@
 package com.itemsharing.itemservice;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ItemserviceApplication {
+import com.itemsharing.itemservice.model.Item;
+import com.itemsharing.itemservice.model.User;
+import com.itemsharing.itemservice.service.ItemService;
+import com.itemsharing.itemservice.service.UserService;
 
+@SpringBootApplication
+public class ItemserviceApplication implements CommandLineRunner{
+
+	@Autowired
+	private ItemService itemService;
+	
+	@Autowired
+	private UserService userService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ItemserviceApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		User user = userService.findByUsername("jadams");
+		
+		Item item1 = new Item();
+		item1.setName("Item1");
+		item1.setItemCondition("New");
+		item1.setStatus("Active");
+		item1.setAddDate(new Date());
+		item1.setDescription("This is an item description.");
+		item1.setUser(user);
+		
+		itemService.addItemByUser(item1, user.getUsername());
+		
+		Item item2 = new Item();
+		item2.setName("Item2");
+		item2.setItemCondition("Used");
+		item2.setStatus("Inactive");
+		item2.setAddDate(new Date());
+		item2.setDescription("This is an item description for item2.");
+		item2.setUser(user);
+		
+		itemService.addItemByUser(item2, user.getUsername());
 	}
 }
--- itemsharing.userservice/src/main/java/com/itemsharing/userservice/UserserviceApplication.java
+++ itemsharing.userservice/src/main/java/com/itemsharing/userservice/UserserviceApplication.java
@@ -1,11 +1,42 @@
 package com.itemsharing.userservice;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.itemsharing.userservice.model.Role;
+import com.itemsharing.userservice.model.User;
+import com.itemsharing.userservice.model.UserRole;
+import com.itemsharing.userservice.service.UserService;
+
 @SpringBootApplication
-public class UserserviceApplication {
+public class UserserviceApplication implements CommandLineRunner{
+	@Autowired
+	private UserService userService;
+	
 	public static void main (String[] args) {
 		SpringApplication.run(UserserviceApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		User user1 = new User();
+		user1.setFirstName("John");
+		user1.setLastName("Adams");
+		user1.setUsername("jadams");
+		user1.setPassword("password");
+		user1.setEmail("jadams@gmail.com");
+		
+		Set<UserRole> userRoles = new HashSet<>();
+		Role role1 = new Role();
+		role1.setRoleId(1);
+		role1.setName("ROLE_USER");
+		userRoles.add(new UserRole(user1, role1));
+		
+		userService.createUser(user1);
 	}
 }
